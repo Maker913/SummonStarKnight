75f1d208e81c4c1ec5c40e43c27815e49480f696
Kazunori Hara <17c1020021hk@edu.tech.ac.jp>
2019-05-06 16:28:33 +0900

    画面遷移の処理

---
+++ Assets/hara/Scripts/testScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    public void ButtonAction()
+    {
+        SceneChanger.instance.LoadScene("test2");
+    }
+}
