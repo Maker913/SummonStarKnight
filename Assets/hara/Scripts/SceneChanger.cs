d942f2d882c7a2bd165f2af61765c7148c5bf9e4
KazunoriHara <17c1020021hk@edu.tech.ac.jp>
2019-05-10 14:00:05 +0900

    シーン遷移、フェード処理（途中）

--- Assets/hara/Scripts/SceneChanger.cs
+++ Assets/hara/Scripts/SceneChanger.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class SceneChanger : MonoBehaviour
 {
     public static SceneChanger instance;
+
+    private Texture2D blackTexture;
+    private float fadeAlpha = 0;
+    private bool isFading = false;
+
     private void Awake()
     {
         if(instance == null)
@@ -16,6 +21,22 @@ public class SceneChanger : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        /*
+        blackTexture = new Texture2D(32, 32, TextureFormat.RGB24, false);
+        blackTexture.ReadPixels(new Rect(0, 0, 32, 32), 0, 0, false);
+        blackTexture.SetPixel(0, 0, Color.white);
+        blackTexture.Apply();
+        */
+    }
+
+    public void OnGUI()
+    {
+        if (!isFading)
+            return;
+
+        GUI.color = new Color(0, 0, 0, fadeAlpha);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), blackTexture);
     }
 
     /// <summary>
