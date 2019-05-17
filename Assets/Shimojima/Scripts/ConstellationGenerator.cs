251fec04b627b8de8be74b78c444bb8aaeaee22d
Shimojima <sigkalra8888@gmail.com>
2019-05-17 11:29:16 +0900

    [Update] 敵の星座を表示するスクリプトの調整

--- Assets/Shimojima/Scripts/ConstellationGenerator.cs
+++ Assets/Shimojima/Scripts/ConstellationGenerator.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ConstellationGenerator : MonoBehaviour
 {
     public GameObject[] constellation;
+    [SerializeField]
+    private string[] register;
+    private int[,] attackConstellation;
     private int constellationLength;
     private float time;
     private int turn;
@@ -12,6 +15,8 @@ public class ConstellationGenerator : MonoBehaviour
     void Start()
     {
         constellationLength = constellation.Length;
+        attackConstellation = new int[register.Length, 14];
+        Register();
         turn = 1;
     }
 
@@ -22,12 +27,34 @@ public class ConstellationGenerator : MonoBehaviour
 
         if (turn == 1 && time >= 3.00f)
         {
-            int cons = Random.Range(0, constellationLength);
-            GameObject consPre = Instantiate(constellation[cons], new Vector3(transform.localPosition.x,
-                                                                              transform.localPosition.y + 2, 
-                                                                              transform.localPosition.z),
-                                                                              Quaternion.identity);
+            
             turn = 2;
         }
+    }
+
+    private void Register()
+    {
+        for (int i = 0; i < register.Length; i++)
+        {
+            if (register[i] != "")
+            {
+                string[] tmp = register[i].Split(',');
+                int[] tmp2 = new int[tmp.Length];
+                for (int j = 0; j < tmp.Length; j++)
+                {
+                    tmp2[j] = int.Parse(tmp[j]);
+                    attackConstellation[i, j] = tmp2[j];
+                }
+            }
+        }
+
+        for (int i = 0; i < 14; i++)
+        {
+            if (attackConstellation[14,i] != 0)
+            {
+                constellation[attackConstellation[14, i] - 1].SetActive(true);
+            }
+        }
+
     }
 }
