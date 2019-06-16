28c1ad72483634a81c33a06064ee8ef2d4c80b96
koyama <youtub8756@gamil.com>
2019-06-17 03:52:45 +0900

    改善

--- Assets/koyama/Script/AudioManager.cs
+++ Assets/koyama/Script/AudioManager.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 public class AudioManager : MonoBehaviour
 {
     /// <summary>
@@ -11,14 +10,13 @@ public class AudioManager : MonoBehaviour
     ///AudioManager.Instance.PlayBGM("番号");
     /// </summary>
     public static AudioManager Instance;
-    private AudioSource audioSource;
     //SEとBGMの区別
     [SerializeField]
     private AudioClip[] BGM;
     private AudioSource BGMsource; 
     [SerializeField]
     private AudioClip[] SE;
-    private AudioSource SEsource;
+    private AudioSource SEsources;
     private void Awake()
     {
         if(Instance == null)
@@ -30,44 +28,60 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        audioSource = gameObject.GetComponent<AudioSource>();
+        SEsources = gameObject.GetComponent<AudioSource>();
+        BGMsource = gameObject.AddComponent<AudioSource>();
+        //BGMループ
         BGMsource.loop = true;
     }
-    //SE
-    public void PlaySE(int number)
-    {
-        if(0 > number || SE.Length <= number)
-        {
-            return;
-        }
-        foreach(AudioClip audio in SE)
-        {
-            if(audioSource.clip == SE[number])
-            {
-                audioSource.clip = audio;
-                audioSource.Play();
-                break;
-            }
-        }
-    }
-    //BGM
+    //BGM再生
     public void PlayBGM(int number)
     {
         if(0 > number || BGM.Length <= number)
         {
             return;
         }
+        //同じBGMの時は何もしない
         if (BGMsource.clip == BGM[number])
         {
+            Debug.Log("何もしない");
             return;
         }
         BGMsource.Stop();
         BGMsource.clip = BGM[number];
         BGMsource.Play();
+        Debug.Log("鳴った");
     }
+    //BGM停止
     public void StopBGM()
     {
         BGMsource.Stop();
         BGMsource.clip = null;
+        Debug.Log("止めた");
+    }
+    //SE再生
+    public void PlaySE(int number)
+    {
+        if (0 > number || SE.Length <= number)
+        {
+            return;
+        }
+        foreach (AudioClip source in SE)
+        {
+            SEsources.clip = SE[number];
+            SEsources.Play();
+            Debug.Log("鳴った");
+            break;
+        }
+    }
+    //SE停止
+    public void StopSE()
+    {
+        //すべてのSEの停止
+        foreach (AudioClip source in SE)
+        {
+            SEsources.Stop();
+            SEsources.clip = null;
+            Debug.Log("止めた");
+        }
     }
 }
