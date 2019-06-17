c33d19f540d0e5cab67b67ff1934510e7032d6bc
koyama <youtub8756@gmail.cpm>
2019-06-17 18:37:25 +0900

    分類分け

---
+++ Assets/koyama/Script/BgmManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgmManager : MonoBehaviour
+{
+    AudioManager audioManager;
+    public static BgmManager Instance;
+    [SerializeField]
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        audioManager = AudioManager.Instance;
+    }
+
+    private void Start()
+    {
+        
+    }
+    //BGM再生
+    public void PlayBGM(int number)
+    {
+        if (0 > number || audioManager.BgmList.Length <= number)
+        {
+            return;
+        }
+        //同じBGMの時は何もしない
+        if (audioSource.clip == audioManager.BgmList[number])
+        {
+            Debug.Log("何もしない");
+            return;
+        }
+        audioSource.Stop();
+        audioSource.clip = audioManager.BgmList[number];
+        audioSource.Play();
+        Debug.Log("鳴った");
+    }
+    //BGM停止
+    public void StopBGM()
+    {
+        audioSource.Stop();
+        audioSource.clip = null;
+        Debug.Log("止めた");
+    }
+}
---
+++ Assets/koyama/Script/SeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeManager : MonoBehaviour
+{
+    public static SeManager Instance;
+    private AudioManager audioManager;
+    private AudioSource[] seSource;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    private void Start()
+    {
+        audioManager = AudioManager.Instance;
+        seSource = new AudioSource[audioManager.SeList.Length];
+        seSource = GetComponents<AudioSource>();
+        for(int i = 0; i < audioManager.SeList.Length; i++)
+        {
+            seSource[i].clip = audioManager.SeList[i];
+        }
+    }
+    //SE再生
+    public void PlaySE(int number)
+    {
+        if (0 > number || audioManager.SeList.Length <= number)
+        {
+            return;
+        }
+        seSource[number].PlayOneShot(seSource[number].clip);
+        Debug.Log("aaaaa");
+    }
+}
