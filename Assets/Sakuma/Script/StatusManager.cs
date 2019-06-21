0066e5f78bd495c747f230d1db1db1f20386ae6f
miiiyan28 <miiiyan.2525@gmail.com>
2019-06-21 14:04:50 +0900

    [Update]星座の能力追加

--- Assets/Sakuma/Script/StatusManager.cs
+++ Assets/Sakuma/Script/StatusManager.cs
@@ -15,15 +15,32 @@ public class StatusManager : MonoBehaviour
 
     [SerializeField]
     private float gageSpeedDef;
+    [SerializeField]
+    private int enemyAtkDef;
+    [SerializeField]
+    private int playerAtkDef;
     public int EnemyActionrange=1;
     //ここから先久野変数
     private int reoSlip;
+    private int scorSlip;
+    public int pisBarrier;
+    private int virgoBarrier;
+    private int LibraTurn;
+    private int aquariTurn;
+    //private int cancerAtk;
+    //private int taurusAtk;
+    //private int sagiAtk;
+    
+
 
 
 
     private void Start()
     {
-        gageSpeed = gageSpeedDef; 
+        gageSpeed = gageSpeedDef;
+        enemyAtk = enemyAtkDef;
+        playerAtk = playerAtkDef;
+
     }
 
     public void SummonCheck(int num)
@@ -34,6 +51,47 @@ public class StatusManager : MonoBehaviour
                 //獅子座を召還した時の処理
                 reoSlip = 3;
                 break;
+            case 1:
+                //うお座
+                pisBarrier = 1;
+                break;
+            case 2:
+                //蟹
+                playerAtk = (int)(playerAtk * 1.5f);
+                break;
+            case 3:
+                //サソリ
+                scorSlip = 3;
+                break;
+            case 4:
+                //双子　gemini 攻撃回数が2回に増える しょうま
+                break;
+            case 5:
+                //山羊　Capri　制限ターン2のびる
+                break;
+            case 6:
+                //乙女
+                virgoBarrier = 3;
+                break;
+            case 7:
+                //天秤
+                LibraTurn = 1;
+                break;
+            case 8:
+                //射手
+                playerAtk *= 3;
+                break;
+            case 9:
+                //水瓶
+                aquariTurn = 1;
+                break;
+            case 10:
+                //牡羊 Aries 制限ターン5のびる
+                break;
+            case 11:
+                //牡牛
+                playerAtk *= 2;
+                break;
             default:
                 break;
         }
@@ -41,16 +99,63 @@ public class StatusManager : MonoBehaviour
 
     public void TurnCheck()
     {
-        if(reoSlip > 0)
+        enemyAtk = enemyAtkDef;
+        gageSpeed = gageSpeedDef;
+        //獅子
+        if (reoSlip > 0)
         {
             reoSlip--;
             enemyHP -= 5;
         }
+        //サソリ
+        if (scorSlip > 0)
+        {
+            scorSlip--;
+            enemyHP -= 5;
+        }
+        //魚
+        if (pisBarrier > 0)
+        {
+            enemyAtk = 0;
+        }
+        //乙女
+        if (virgoBarrier > 0)
+        {
+            enemyAtk = 0;
+        }
+        //天秤
+        if(LibraTurn > 0)
+        {
+            gageSpeed += 3;
+        }
+        //水瓶
+        if (aquariTurn > 0)
+        {
+            enemyAtk /= 2;
+            aquariTurn--;
+        }
+
+        //敵
+        if (data > 0)
+        {
+            gageSpeed -= 2;
+            data--;
+        }
+
     }
 
 
     public void BarrierCheck()
     {
+        //魚
+        if(pisBarrier > 0) {
+            pisBarrier--;
+        }
+        //乙女
+        if (virgoBarrier > 0)
+        {
+            virgoBarrier--;
+        }
 
     }
 
@@ -74,7 +179,7 @@ public class StatusManager : MonoBehaviour
         switch (num)
         {
             case 1:
-                //操作時間減少
+                //操作時間減少　魚
                 data = 2;
                 break;
             default:
@@ -89,12 +194,8 @@ public class StatusManager : MonoBehaviour
 
     public void EnemyTurnCheck()
     {
-        gageSpeed = gageSpeedDef;
-        if (data > 0)
-        {
-            gageSpeed -=2;
-            data--;
-        }
+        
+        
 
 
     }
