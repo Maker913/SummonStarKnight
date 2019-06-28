b8dfc1a904379b96e1e79e597d1a3f7c67f61625
Kazunori Hara <17c1020021hk@edu.tech.ac.jp>
2019-06-28 14:17:16 +0900

    [New]スタート画面のカメラ回転

---
+++ Assets/Hara/Scripts/Title/TitleSubCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSubCamera : MonoBehaviour
+{
+    private Camera stageCamera;
+    public RenderTexture CameraRender { set { stageCamera.targetTexture = value; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stageCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stageCamera != null)
+        {
+            
+        }
+    }
+}
