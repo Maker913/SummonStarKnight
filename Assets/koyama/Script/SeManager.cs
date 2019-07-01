8ae730268e896344ca63f359c09597ed6a321f6e
koyama <>
2019-07-01 16:48:17 +0900

    漏れ

--- Assets/koyama/Script/SeManager.cs
+++ Assets/koyama/Script/SeManager.cs
@@ -12,12 +12,10 @@ public class SeManager : SoundModel
     public void PlaySE(int number)
     {
         audioSource.PlayOneShot(audioClips[number]);
-        Debug.Log("aaaaa");
     }
     // SE停止
     public void StopSE()
     {
         audioSource.Stop();
-        Debug.Log("se止めた");
     }
 }
