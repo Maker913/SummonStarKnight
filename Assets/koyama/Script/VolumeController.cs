9077113a359b9663fecf59bd855008d2f3b765b8
koyama <>
2019-07-12 13:58:04 +0900

    調整

---
+++ Assets/koyama/Script/VolumeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeController : MonoBehaviour
+{
+    private AudioMixer audioMixer;
+    public void SetMaster(float volume)
+    {
+        audioMixer.SetFloat("Master", volume);
+    }
+    public void SetBGM(float volume)
+    {
+        audioMixer.SetFloat("BGM", volume);
+    }
+    public void SetSE(float volume)
+    {
+        audioMixer.SetFloat("SE", volume);
+    }
+}
